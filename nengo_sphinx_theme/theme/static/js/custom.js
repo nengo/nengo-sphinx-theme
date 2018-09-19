c8214764b78f44d12ea900f1b321b37478b2598a
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2018-09-19 10:53:15 -0300

    Fix sidebar affix

--- nengo_sphinx_theme/theme/static/js/custom.js
+++ nengo_sphinx_theme/theme/static/js/custom.js
@@ -1,81 +1,98 @@
-/**
-* This JS file is for additional new JS built over the existing theme
-* ...so as to avoid breaking anything unexpectedly
-*/
-(function($) {
+$(document).ready(function() {
 
-  var $body = $('body');
+  // Dropdown menu
+  var triggers = document.querySelectorAll(
+    '.main-nav li.menu-item-has-children'
+  );
+  var background = document.querySelector('.dropdownBackground');
+  var nav = document.querySelector('.main-nav');
 
-  $(document).ready(function() {
-
-    /**
-    * Dropdown menu
-    */
-    var triggers = document.querySelectorAll(
-      '.main-nav li.menu-item-has-children'
-    );
-    var background = document.querySelector('.dropdownBackground');
-    var nav = document.querySelector('.main-nav');
-
-    function handleEnter() {
-      this.classList.add('trigger-enter');
-      setTimeout(
-        () =>
+  function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(
+      () =>
         this.classList.contains('trigger-enter') &&
         this.classList.add('trigger-enter-active'),
-        150
-      );
-      background.classList.add('open');
-
-      var dropdown = this.querySelector('.main-nav .sub-menu');
-      var dropdownCoords = dropdown.getBoundingClientRect();
-      var navCoords = nav.getBoundingClientRect();
-
-      var coords = {
-        height: dropdownCoords.height,
-        width: dropdownCoords.width,
-        top: dropdownCoords.top - navCoords.top + 5,
-        left: dropdownCoords.left - navCoords.left
-      };
-
-      background.style.setProperty('width', `${coords.width}px`);
-      background.style.setProperty('height', `${coords.height}px`);
-      background.style.setProperty(
-        'transform',
-        `translate(${coords.left}px, ${coords.top}px)`
-      );
-    }
-
-    function handleLeave() {
-      this.classList.remove('trigger-enter', 'trigger-enter-active');
-      background.classList.remove('open');
-    }
-
-    triggers.forEach(trigger =>
-      trigger.addEventListener('mouseenter', handleEnter)
+      150
     );
-    triggers.forEach(trigger =>
-      trigger.addEventListener('mouseleave', handleLeave)
+    background.classList.add('open');
+
+    var dropdown = this.querySelector('.main-nav .sub-menu');
+    var dropdownCoords = dropdown.getBoundingClientRect();
+    var navCoords = nav.getBoundingClientRect();
+
+    var coords = {
+      height: dropdownCoords.height,
+      width: dropdownCoords.width,
+      top: dropdownCoords.top - navCoords.top + 5,
+      left: dropdownCoords.left - navCoords.left
+    };
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty(
+      'transform',
+      `translate(${coords.left}px, ${coords.top}px)`
     );
+  }
 
-    // Mobile slide out menus
-    $('#mobile-nav').mmenu({
-      slidingSubmenus: false,
-      offCanvas: {
-        position: 'right'
-      }
-    });
-    var MobileNavAPI = $('#mobile-nav').data('mmenu');
-    $('#mobile-close-button').click(function() {
-      MobileNavAPI.close();
-    });
+  function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+  }
 
-    /**
-    * Set height on sidebar
-    */
-    var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    var $sidebar = $('.bs-docs-sidebar');
-    $sidebar.css('max-height', `${viewportHeight - 40}px`);
+  triggers.forEach(trigger =>
+                   trigger.addEventListener('mouseenter', handleEnter)
+                  );
+  triggers.forEach(trigger =>
+                   trigger.addEventListener('mouseleave', handleLeave)
+                  );
+
+  // Mobile slide out menus
+  $('#mobile-nav').mmenu({
+    slidingSubmenus: false,
+    offCanvas: {
+      position: 'right'
+    }
+  });
+  var MobileNavAPI = $('#mobile-nav').data('mmenu');
+  $('#mobile-close-button').click(function() {
+    MobileNavAPI.close();
   });
 
-})(jQuery);
+  // Set height on sidebar
+  var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var $sidebar = $('.bs-docs-sidebar');
+  $sidebar.css('max-height', `${viewportHeight - 40}px`);
+
+  // Set up affix and scrollspy
+  var $navAffix = $('#nav-affix');
+  var $contentEl = $('.wrapper-content-right');
+
+  // Desktop only
+  if( !$('#mobile-menu-toggler').is(':visible') ) {
+    // Only if the content is taller than the sidebar,
+    // to avoid scroll jerk
+    if ( $contentEl.height() > $navAffix.height() ) {
+      // Init
+      $navAffix.affix({
+        offset: {
+          top: 100,
+          bottom: function () {
+            return (this.bottom = $('footer').outerHeight(true) + 200)
+          }
+        }
+      });
+      // Recalculate the sidebar
+      $(window).resize(function() {
+        $navAffix.affix('checkPosition');
+      });
+    }
+    $('body').scrollspy();
+  } else {
+    $('.wrapper-navleft').hide();
+    $('#mobile-menu-toggler').click(function() {
+      $('.wrapper-navleft').toggle();
+    });
+  }
+});
