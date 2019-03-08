8d104b931a82aaff570501cc555e9c13929949c1
Trevor Bekolay <tbekolay@gmail.com>
2019-03-08 11:06:10 -0500

    Release version 0.8.0

--- nengo_sphinx_theme/version.py
+++ nengo_sphinx_theme/version.py
@@ -7,7 +7,7 @@ a release version. Release versions are git tagged with the version.
 """
 
 name = "nengo_sphinx_theme"
-version_info = (0, 7, 0)  # (major, minor, patch)
+version_info = (0, 8, 0)  # (major, minor, patch)
 dev = None
 
 version = "{v}{dev}".format(v='.'.join(str(v) for v in version_info),
