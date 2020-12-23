88c9576df0d5f4c713711349ebfcae63a5de4fd2
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2020-12-23 14:46:26 -0500

    Remove Python 3.5 classifier

--- setup.py
+++ setup.py
@@ -77,7 +77,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development",
